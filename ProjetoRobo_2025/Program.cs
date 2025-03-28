72399436031897979a55acdd0e2c2d35e5d5b9f2
leandroJader <ldandro>
2025-03-28 16:56:12 -0300

    implementado loop para realização correta do funcionamento do robo conforme os comandos recebidos

--- ProjetoRobo_2025/Program.cs
+++ ProjetoRobo_2025/Program.cs
@@ -6,81 +6,87 @@
         {
             Console.WriteLine("Projeto Robo_2025");
 
-            int[] posicaoRobo = [0, 0];
+            int[] posicaoRobo = [1, 2];
             char OrientacaoRobo = 'n';
 
-            char comando = 'm';
+            Console.WriteLine("informe as coordenadas que deseja passar para o robo");
+
+            char [] comando = Console.ReadLine()!.ToLower().ToCharArray();
+
+
             //movimento para frente olhando ao norte
 
-
-            if (comando=='m' && OrientacaoRobo == 'n')
+            foreach (char i in comando)
             {
-                posicaoRobo[1] += 1;
+
+                if (i == 'm' && OrientacaoRobo == 'n')
+                {
+                    posicaoRobo[1] += 1;
+                }
+                // movimento para trás
+                else if (i == 'm' && OrientacaoRobo == 's')
+                {
+                    posicaoRobo[1] -= 1;
+                }
+                // o robo anda para a direita
+                else if (i == 'm' && OrientacaoRobo == 'l')
+                {
+                    posicaoRobo[0] += 1;
+                }
+                // o robo anda para a esquerda
+                else if (i == 'm' && OrientacaoRobo == 'o')
+                {
+                    posicaoRobo[0] -= 1;
+                }
+
+
+
+                //o robo vira para a direita
+                if (i == 'd' && OrientacaoRobo == 'n')
+                {
+                    OrientacaoRobo = 'l';
+                }
+
+                else if (   i == 'd' && OrientacaoRobo == 's')
+                {
+                    OrientacaoRobo = 'o';
+                }
+
+                else if (i == 'd' && OrientacaoRobo == 'l')
+                {
+                    OrientacaoRobo = 's';
+                }
+
+                else if (i == 'd' && OrientacaoRobo == 'o')
+                {
+                    OrientacaoRobo = 'n';
+                }
+
+
+                //o robo vira para a esquerda 
+                if (i == 'e' && OrientacaoRobo == 'n')
+                {
+                    OrientacaoRobo = 'o';
+                }
+
+
+                else if (i == 'e' && OrientacaoRobo == 's')
+                {
+                    OrientacaoRobo = 'l';
+                }
+
+                else if (i == 'e' && OrientacaoRobo == 'l')
+                {
+                    OrientacaoRobo = 'n';
+                }
+
+                else if (i == 'e' && OrientacaoRobo == 'o')
+                {
+                    OrientacaoRobo = 's';
+                }
             }
-            // movimento para trás
-            else if (comando=='m'&& OrientacaoRobo=='s')
-            {
-                posicaoRobo[1] -= 1;
-            }
-            // o robo anda para a direita
-            else if (comando == 'm' && OrientacaoRobo == 'l')
-            {
-                posicaoRobo[0] += 1;
-            }
-            // o robo anda para a esquerda
-            else if (comando=='m' && OrientacaoRobo == 'o')
-            {
-                posicaoRobo[0] -= 1;
-            }
-
-
-
-            //o robo vira para a direita
-            if (comando == 'd' && OrientacaoRobo == 'n')
-            {
-                OrientacaoRobo = 'l';
-            }
-
-            else if (comando == 'd' && OrientacaoRobo == 's')
-            {
-                OrientacaoRobo = 'o';
-            }
-
-            else if (comando == 'd' && OrientacaoRobo == 'l')
-            {
-                OrientacaoRobo = 's';
-            }
-
-            else if (comando == 'd' && OrientacaoRobo == 'o')
-            {
-                OrientacaoRobo = 'n';
-            }
-
-
-            //o robo vira para a esquerda 
-            if (comando == 'e' && OrientacaoRobo == 'n')
-            {
-                OrientacaoRobo = 'o';
-            }
-
-
-            else if (comando == 'e' && OrientacaoRobo == 's')
-            {
-                OrientacaoRobo = 'l';
-            }
-
-            else if (comando == 'e' && OrientacaoRobo == 'l')
-            {
-                OrientacaoRobo = 'n';
-            }
-
-            else if (comando == 'e' && OrientacaoRobo == 'o')
-            {
-                OrientacaoRobo = 's';
-            }
-
-           
-
+            Console.WriteLine($"o robo esta na posição x{posicaoRobo[0]} {posicaoRobo[1]}y olhando para :{OrientacaoRobo}");
+            Console.ReadLine();
 
         }
     }
