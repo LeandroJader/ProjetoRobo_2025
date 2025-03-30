bbcfa0b1f9e23c7925777d9ed771fde97797651e
leandroJader <ldandro>
2025-03-29 21:49:35 -0300

    implementada nova refatoração de código, atribuindo novas  funções para uma melhor vizualização de codigo e implementada a primeira versão do segundo robo

--- ProjetoRobo_2025/Program.cs
+++ ProjetoRobo_2025/Program.cs
@@ -8,83 +8,21 @@
 
             int[] posicaoRobo = [1, 2];
             char OrientacaoRobo = 'n';
-
-           char[] comando = funcoesRobo.coordenadasRobo();
-
-
-            //movimento para frente olhando ao norte
-
-            foreach (char i in comando)
-            {
-
-                if (i == 'm' )
-                {
-                    funcoesRobo.MovimentarRobo(OrientacaoRobo,posicaoRobo);
-                }
-                // movimento para trás
-                else if (i == 'm')
-                {
-                    funcoesRobo.MovimentarRobo(OrientacaoRobo, posicaoRobo);
-                }
-                // o robo anda para a direita
-                else if (i == 'm' )
-                {
-                    funcoesRobo.MovimentarRobo(OrientacaoRobo, posicaoRobo);
-                }
-                // o robo anda para a esquerda
-                else if (i == 'm' )
-                {
-                    funcoesRobo.MovimentarRobo(OrientacaoRobo, posicaoRobo);
-                }
+            int[] PosicaoRobo2 = [3, 3];
+            char orientacaoRobo2 = 'l';
 
 
-                //o robo vira para a direita
-                if (i == 'd' && OrientacaoRobo == 'n')
-                {
-                    OrientacaoRobo = 'l';
-                }
+            char[] comando = funcoesRobo.coordenadasRobo();
 
-                else if (   i == 'd' && OrientacaoRobo == 's')
-                {
-                    OrientacaoRobo = 'o';
-                }
-
-                else if (i == 'd' && OrientacaoRobo == 'l')
-                {
-                    OrientacaoRobo = 's';
-                }
-
-                else if (i == 'd' && OrientacaoRobo == 'o')
-                {
-                    OrientacaoRobo = 'n';
-                }
+            funcoesRobo.MovimentoRobo(comando, posicaoRobo, OrientacaoRobo);
+            funcoesRobo.MostrarPosicionamentoRobo(posicaoRobo, OrientacaoRobo);
 
 
-                //o robo vira para a esquerda 
-                if (i == 'e' && OrientacaoRobo == 'n')
-                {
-                    OrientacaoRobo = 'o';
-                }
+            char[] comandoRobo2 = funcoesRobo.coordenadasRobo();
 
-
-                else if (i == 'e' && OrientacaoRobo == 's')
-                {
-                    OrientacaoRobo = 'l';
-                }
-
-                else if (i == 'e' && OrientacaoRobo == 'l')
-                {
-                    OrientacaoRobo = 'n';
-                }
-
-                else if (i == 'e' && OrientacaoRobo == 'o')
-                {
-                    OrientacaoRobo = 's';
-                }
-            }
-            Console.WriteLine($"o robo esta na posição x{posicaoRobo[0]} {posicaoRobo[1]}y olhando para :{OrientacaoRobo}");
-            Console.ReadLine();
-
+            funcoesRobo.MovimentoRobo(comandoRobo2, PosicaoRobo2, orientacaoRobo2);
+            funcoesRobo.MostrarPosicionamentoRobo(PosicaoRobo2, orientacaoRobo2);
+            
         }
     }
 }
--- ProjetoRobo_2025/funcoesRobo.cs
+++ ProjetoRobo_2025/funcoesRobo.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 namespace ProjetoRobo_2025;
 public class funcoesRobo
 {
@@ -11,17 +13,89 @@ public class funcoesRobo
         return comando;
 
     }
-        public static void MovimentarRobo(char orientacao,int[] posicaoRobo)
+    public static void MovimentoRobo(char[] comando, int[] posicaoRobo, char OrientacaoRobo)
+
     {
-        switch (orientacao)
+
+        foreach (char i in comando)
         {
-            case 'n': posicaoRobo[1] += 1; break;
-            case 's': posicaoRobo[1] -= 1; ; break;
-            case 'l': posicaoRobo[0] += 1;  break;
-            case 'o': posicaoRobo[0] -= 1; break;
+
+            if (i == 'm' && OrientacaoRobo == 'n')
+            {
+                posicaoRobo[1] += 1;
+            }
+            // movimento para trás
+            else if (i == 'm' && OrientacaoRobo == 's')
+            {
+                posicaoRobo[1] -= 1;
+            }
+            // o robo anda para a direita
+            else if (i == 'm' && OrientacaoRobo == 'l')
+            {
+                posicaoRobo[0] += 1;
+            }
+            // o robo anda para a esquerda
+            else if (i == 'm' && OrientacaoRobo == 'o')
+            {
+                posicaoRobo[0] -= 1;
+            }
+
+
+
+            //o robo vira para a direita
+            if (i == 'd' && OrientacaoRobo == 'n')
+            {
+                OrientacaoRobo = 'l';
+            }
+
+            else if (i == 'd' && OrientacaoRobo == 's')
+            {
+                OrientacaoRobo = 'o';
+            }
+
+            else if (i == 'd' && OrientacaoRobo == 'l')
+            {
+                OrientacaoRobo = 's';
+            }
+
+            else if (i == 'd' && OrientacaoRobo == 'o')
+            {
+                OrientacaoRobo = 'n';
+            }
+
+
+            //o robo vira para a esquerda 
+            if (i == 'e' && OrientacaoRobo == 'n')
+            {
+                OrientacaoRobo = 'o';
+            }
+
+
+            else if (i == 'e' && OrientacaoRobo == 's')
+            {
+                OrientacaoRobo = 'l';
+            }
+
+            else if (i == 'e' && OrientacaoRobo == 'l')
+            {
+                OrientacaoRobo = 'n';
+            }
+
+            else if (i == 'e' && OrientacaoRobo == 'o')
+            {
+                OrientacaoRobo = 's';
+            }
         }
+    }
+    public static void MostrarPosicionamentoRobo(int[] posicaoRobo, char OrientacaoRobo)
+    {
 
+        Console.WriteLine($"o robo esta na posição x{posicaoRobo[0]} {posicaoRobo[1]}y olhando para :{OrientacaoRobo}");
+        Console.ReadLine();
 
+    }
+    public static void MostrarPosicionamentoRobo2(int[] posicaoRobo, char OrientacaoRobo)
+    {
 
     }
 
